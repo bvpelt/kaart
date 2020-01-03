01ac80f1adc122913a8e1ae524dcef73f6278a03
PeltBartvan <bart.vanpelt@kadaster.nl>
2020-01-03 16:31:18 +0100

    Try again

--- kaart/src/app/map/map.component.ts
+++ kaart/src/app/map/map.component.ts
@@ -16,37 +16,69 @@ import WMTSTileGrid from 'ol/tilegrid/WMTS';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements AfterViewInit {
+
+
   /* WMTS PDOK */
-  //static projection = 'EPSG:28992';
+  /* static projection = 'EPSG:28992'; */
 
   static map: Map;
   static projectionExtent = [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999];
-  static projection: Projection = new Projection({code: 'EPSG:28992', units: 'm', extent: MapComponent.projectionExtent});
+  static projection: Projection = new Projection({
+    code: 'EPSG:28992',
+    units: 'm',
+    extent: MapComponent.projectionExtent
+  });
 
-  static size = getWidth(MapComponent.projectionExtent) / 256;
+  static size: number = getWidth(MapComponent.projectionExtent) / 256;
 
-  static wmtsSource: WMTS = null;
-  static resolutions: any;
-  static matrixIds: any;
+  private wmtsSource: WMTS = null;
+  /*  private resolutions: any; */
 
-  constructor () {
-    MapComponent.resolutions = new Array(14);
-    MapComponent.matrixIds = new Array(14);
-    for (let z = 0; z < 14; ++z) {
-      MapComponent.resolutions[z] = MapComponent.size / Math.pow(2, z);
-      MapComponent.matrixIds[z] = z; //'EPSG:28992:' + z;
-      // console.log("z: " + z + " resolution: " + this.resolutions[z] + " matrixIds: " + this.matrixIds[z])
+  /* private matrixIds: any; */
+
+  constructor() {
+    if (this.wmtsSource === null) {
+      this.wmtsSource = MapComponent.createWmts();
     }
   }
 
-  ngAfterViewInit() {
+  private static createWmts(): WMTS {
+    const resolutions: Array<number> = new Array(14);
+    const matrixIds: Array<string> = new Array(14);
+    const projectionExtent = [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999];
 
+    for (let z = 0; z < 14; ++z) {
+      resolutions[z] = MapComponent.size / Math.pow(2, z);
+      matrixIds[z] = 'EPSG:28992:' + z; /* z */
+      console.log('z: ' + z + ' resolution: ' + resolutions[z] + ' matrixIds: ' + matrixIds[z]);
+    }
+
+    const wmtsSource: WMTS = new WMTS({
+      url: 'https://geodata.nationaalgeoregister.nl/tiles/service/wmts?',
+      layer: 'bgtachtergrond',
+      matrixSet: 'EPSG:28992', /* this.projection, */
+      format: 'image/png',
+      projection: this.projection,
+      tileGrid: new WMTSTileGrid({
+        origin: getTopLeft(projectionExtent),
+        resolutions,
+        matrixIds,
+      }),
+      style: 'default',
+      wrapX: true,
+      crossOrigin: 'anonymous'
+    });
+
+    return wmtsSource;
+  }
+
+  ngAfterViewInit() {
     MapComponent.map = new Map({
       target: 'map',
       layers: [
         new TileLayer({
           opacity: 1.0,
-          source: this.createWmts()
+          source: this.wmtsSource
         })
       ],
       view: new View({
@@ -55,31 +87,10 @@ export class MapComponent implements AfterViewInit {
       }),
       controls: defaultControls().extend([
         new ZoomToExtent({
-          extent: MapComponent.projectionExtent
+          extent: [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999]
         })
       ])
     });
-  }
-
-  private createWmts(): WMTS {
-
-    MapComponent.wmtsSource = new WMTS({
-      url: 'https://geodata.nationaalgeoregister.nl/tiles/service/wmts?',
-      layer: "bgtachtergrond",
-      matrixSet: 'EPSG:28992', //this.projection,
-      format: 'image/png',
-      projection: MapComponent.projection,
-      tileGrid: new WMTSTileGrid({
-        origin: getTopLeft(MapComponent.projectionExtent),
-        resolutions: MapComponent.resolutions,
-        matrixIds: MapComponent.matrixIds,
-      }),
-      style: 'default',
-      wrapX: true,
-      crossOrigin: 'anonymous'
-    });
-
-    return MapComponent.wmtsSource;
   }
 
 }
